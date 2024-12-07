eeadef94e04240f19f0dd3db053411b37031a5e5
BlackPri0r <lawrenceli630@gmail.com>
2024-12-07 20:58:48 +1300

    TESTING: icon cloud

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Reveal from "./components/RevealText";
 import CustomCursor from "./components/CustomCursor";
+import IconCloud from "./components/DynamicIconCloud";
 
 export default function Home() {
   return (
@@ -8,6 +9,33 @@ export default function Home() {
       <div className="h-dvh w-full justify-center flex items-center">
         <Reveal className="text-black text-center text-9xl">Lawrence Li</Reveal>
         {/* <CustomCursor></CustomCursor> */}
+        <IconCloud iconSlugs={
+        ["wix", 
+          "react", 
+          "typescript", 
+          "python", 
+          "godot", 
+          "github", 
+          "godotengine", 
+          "dotnet",
+          "git",
+          "java",
+          "c",
+          "leagueoflegends",
+          "tailwindcss",
+          "html5",
+          "css3",
+          "javascript",
+          "trello",
+          "jira",
+          "unity",
+          "csharp",
+          "cplusplus",
+          "linux",
+          "dota2",
+          "ubuntu",
+          "flask"
+        ]}/>
       </div>
     </div>
   );
